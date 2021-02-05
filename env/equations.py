c4bb6156cfe14567cd08db27ec96005ee71827f0
Kleydson Stenio <kleydson.stenio@gmail.com>
2021-02-05 18:25:45 -0300

    LIBSsa version: 2.0 alpha 13
    equations.py
    * Created functions for Lorentz shape (normal, fixed center and asymmetric variants)
    * Created residuals function
    
    libssa.ui
    * Updated version: 2.0a13

--- env/equations.py
+++ env/equations.py
@@ -19,13 +19,84 @@
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 
-from numpy import empty, array, sum
+from numpy import ndarray, array, empty, sum, std
 
-def lorentz(x, *p):
-	peaks = len(p) // 3
-	lo = empty((len(x), peaks))
+# Lorentzian functions
+def lorentz(x: ndarray, *args: float, **kwargs) -> ndarray:
+	"""
+	:param x: Input vector (wavelength for isolated region)
+	:param args: Parameters of function. These values can be optimized for fit.
+	:param kwargs: Extra fixed parameters (number of peaks, center, asymmetry)
+	:return: y values for function (intensities)
+	"""
+	peaks = kwargs['Peaks']
+	lo = empty((x.size, peaks))
 	for i in range(peaks):
-		h, w, c = p[3 * i], p[(3 * i) + 1], p[(3 * i) + 2]
+		h, w, c = args[3 * i], args[(3 * i) + 1], args[(3 * i) + 2]
 		lo[:, i] = abs(h) / (1 + ((x - c) / (0.5 * w)) ** 2)
 	return sum(lo, 1)
+
+def lorentz_fixed_center(x: ndarray, *args: float, **kwargs) -> ndarray:
+	"""
+	:param x: Input vector (wavelength for isolated region)
+	:param args: Parameters of function. These values can be optimized for fit.
+	:param kwargs: Extra fixed parameters (number of peaks, center, asymmetry)
+	:return: y values for function (intensities)
+	"""
+	peaks, c = kwargs['Peaks'], kwargs['Center']
+	lofc = empty((x.size, peaks))
+	for i in range(peaks):
+		h, w = args[2 * i], args[(2 * i) + 1]
+		lofc[:, i] = abs(h) / (1 + ((x - c[i]) / (0.5 * w)) ** 2)
+	return sum(lofc, 1)
+
+def lorentz_asymmetric(x: ndarray, *args: float, **kwargs) -> ndarray:
+	"""
+	:param x: Input vector (wavelength for isolated region)
+	:param args: Parameters of function. These values can be optimized for fit.
+	:param kwargs: Extra fixed parameters (number of peaks, center, asymmetry)
+	:return: y values for function (intensities)
+	"""
+	peaks = kwargs['Peaks']
+	loa = empty((x.size, peaks))
+	for i in range(peaks):
+		h, w, c, m = args[4 * i], args[(4 * i) + 1], args[(4 * i) + 2], args[(4 * i) + 3]
+		loa[x <= c, i] = abs(h) / (1 + ((x[x <= c] - c) / (0.5 * w * m)) ** 2)
+		loa[x > c, i] = abs(h) / (1 + ((x[x > c] - c) / (0.5 * w * m)) ** 2)
+	return sum(loa, 1)
+
+def lorentz_asymmetric_fixed_center(x: ndarray, *args: float, **kwargs) -> ndarray:
+	"""
+	:param x: Input vector (wavelength for isolated region)
+	:param args: Parameters of function. These values can be optimized for fit.
+	:param kwargs: Extra fixed parameters (number of peaks, center, asymmetry)
+	:return: y values for function (intensities)
+	"""
+	peaks, c = kwargs['Peaks'], kwargs['Center']
+	loafc = empty((x.size, peaks))
+	for i in range(peaks):
+		h, w, m = args[3 * i], args[(3 * i) + 1], args[(3 * i) + 2]
+		loafc[x <= c[i], i] = abs(h) / (1 + ((x[x <= c[i]] - c) / (0.5 * w * m)) ** 2)
+		loafc[x > c[i], i] = abs(h) / (1 + ((x[x > c[i]] - c[i]) / (0.5 * w * m)) ** 2)
+	return sum(loafc, 1)
+
+def lorentz_asymmetric_fixed_center_asymmetry(x: ndarray, *args: float, **kwargs) -> ndarray:
+	"""
+	:param x: Input vector (wavelength for isolated region)
+	:param args: Parameters of function. These values can be optimized for fit.
+	:param kwargs: Extra fixed parameters (number of peaks, center, asymmetry)
+	:return: y values for function (intensities)
+	"""
+	peaks, c, m = kwargs['Peaks'], kwargs['Center'], kwargs['Asymmetry']
+	loafca = empty((x.size, peaks))
+	for i in range(peaks):
+		h, w = args[2 * i], args[(2 * i) + 1]
+		loafca[x <= c[i], i] = abs(h) / (1 + ((x[x <= c[i]] - c) / (0.5 * w * m[i])) ** 2)
+		loafca[x > c[i], i] = abs(h) / (1 + ((x[x > c[i]] - c[i]) / (0.5 * w * m[i])) ** 2)
+	return sum(loafca, 1)
+
+# Residuals function
+def residuals(guess, x, y, shape_id, **kwargs):
+	function_kwargs = {'Center': kwargs['Center'], 'Extra': kwargs['Extra']}
+	return y - kwargs[shape_id](x, *guess, **function_kwargs)
 
